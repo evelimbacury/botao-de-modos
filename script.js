affd4cc80c2165b69a5e070902d4b3d0e9b5cbca
evelimbacury <evelimbacury@gmail.com>
2022-02-22 21:53:36 -0400

    Adicionando arquivos ao git

---
+++ script.js
@@ -0,0 +1,20 @@
+function modoClaro(){
+    //cor fundo
+    document.body.style.backgroundColor="#fff";
+    //cor fundo sol
+    document.getElementById("botao").style.backgroundColor="#fff";
+    //cor sol
+    document.getElementById("botao").style.color = "#ffb224"
+    //posição sol
+    document.getElementById("containerIcons").style.marginLeft = "-153px"
+}
+function modoEscuro(){
+    //cor fundo
+    document.body.style.backgroundColor="#101013";
+    //cor fundo lua
+    document.getElementById("botao").style.backgroundColor = "#101013"
+    //cor lua
+    document.getElementById("botao").style.color="#fff";
+    //posição container
+    document.getElementById("containerIcons").style.marginLeft = "-5px"
+}
